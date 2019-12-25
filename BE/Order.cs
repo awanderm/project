cdfad3e4af9dffe566c7910581fd34bfaabf0649
awanderm <ayalav5@gmail.com>
2019-12-25 14:25:18 +0200

    initial commit

---
+++ BE/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Order
+    {
+        int HostingUnitKey;
+        int GuestRequestKey;
+        int OrderKey;
+        string Status;
+        DateTime CreateDate;
+        DateTime OrderDate;
+        public override string ToString()
+        {
+            return "";
+        }
+    }
+}
---
+++ DAL/Dal_imp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Dal_imp : Idal
+    {
+        // public function?
+        void addRequest(BE.GuestRequest g)
+        {
+            BE.GuestRequest e=(DS.G).FirstOrDefault(m => g.GuestRequestKey == m.GuestRequestKey);
+            if(e!=null)
+            {
+                throw new Exception("הבקשה כבר קיימת במערכת");
+            }
+            DS.G.Add(e.Clone()); // clone???
+        }
+        void updateRequest(int requestKey, string myStatus);
+        void addUnit(BE.HostingUnit u);
+        void eraseUnit(int unitKey);
+        void updateUnit(BE.HostingUnit u);
+        void addOrder(BE.Order o);
+        void updateOrder(int orderKey);
+        IEnumerable<BE.HostingUnit> getUnits();
+        IEnumerable<BE.GuestRequest> getRequests();
+        IEnumerable<BE.Order> getOrders();
+        IEnumerable<BE.BankBranch> getBranches();
+    }
+}
